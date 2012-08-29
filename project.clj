19c628f5321db63700efc5c27237e2a455225278
Phil Hagelberg <technomancy@gmail.com>
2012-08-29 14:47:56 -0700

    use Leiningen 2; bump dependencies.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Just for laughs"
   :url "http://chortles.herokuapp.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring/ring-jetty-adapter "1.0.0"]
-                 [com.cemerick/drawbridge "0.0.3"]
-                 [ring-basic-authentication "1.0.1"]])
+                 [ring/ring-jetty-adapter "1.1.0"]
+                 [com.cemerick/drawbridge "0.0.6"]
+                 [ring-basic-authentication "1.0.1"]]
+  :min-lein-version "2.0.0")

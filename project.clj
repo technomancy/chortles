f44f2abbdaf35f1f3decb9a0923f85168e966943
priyatam <priyatam@gmail.com>
2013-11-27 14:44:37 -0800

    added main method

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
                  [ring/ring-jetty-adapter "1.1.6"]
                  [com.cemerick/drawbridge "0.0.6"]
                  [ring-basic-authentication "1.0.1"]]
-  :min-lein-version "2.0.0")
+  :min-lein-version "2.0.0"
+  :main chortles.web)

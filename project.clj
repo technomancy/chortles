a927ca81413e0848125bf2dea26787d3375b65db
priyatam <priyatam@gmail.com>
2013-11-27 12:56:40 -0800

    updated versions of clj, jetty

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject chortles "1.0.0"
   :description "Just for laughs"
   :url "http://chortles.herokuapp.com"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring/ring-jetty-adapter "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-jetty-adapter "1.1.6"]
                  [com.cemerick/drawbridge "0.0.6"]
                  [ring-basic-authentication "1.0.1"]]
   :min-lein-version "2.0.0")
